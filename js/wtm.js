3a1a5fad6704826a7837ec396330a6b7f9312dd1
wtmalicante <37121396+wtmalicante@users.noreply.github.com>
2018-03-07 00:51:48 +0100

    Create wtm.js

---
+++ js/wtm.js
@@ -0,0 +1,10 @@
+	/* ----------------------------------------------------------- */
+	/*  Event counter 
+	/* -----------------------------------------------------------*/
+
+	if ( $( '.countdown' ).length > 0 ) {
+		$(".countdown").jCounter({
+		  	date: '10 March 2018 00:00:00',
+		  	fallback: function() { console.log("count finished!") }
+		});
+	}
