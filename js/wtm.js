d30573a30b55f280ab2eae9cac4f958b651b477b
wtmalicante <37121396+wtmalicante@users.noreply.github.com>
2019-04-19 18:49:04 +0200

    Update wtm.js

--- js/wtm.js
+++ js/wtm.js
@@ -6,7 +6,7 @@ jQuery(function($) {
 
 	if ( $( '.countdown' ).length > 0 ) {
 		$(".countdown").jCounter({
-		  	date: '14 april 2018 00:00:00',
+		  	date: '11 may 2019 00:00:00',
 		  	fallback: function() { console.log("count finished!") }
 		});
 	}
